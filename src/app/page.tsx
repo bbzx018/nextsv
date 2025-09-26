334e473a4d55f126035f94b73097520609abbad2
Bbzx <bookbig089@gmail.com>
2025-09-26 19:02:56 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-grey-100 via-grey-200 to-grey-300 p-8">
-      <h1 className="text-4xl font-extrabold text-white-700 mb-10 text-center drop-shadow-md">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-green-100 via-green-200 to-green-300 p-8">
+      <h1 className="text-4xl font-extrabold text-yellow-700 mb-10 text-center drop-shadow-md">
         👼 Welcome to the Menu 👼
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-xl">
         <LinkButton href="/post" label="📄 Posts (Server Component)" />
         <LinkButton href="/user" label="👩‍💻 Users (Client Component)" />
-        <LinkButton href="/post-client" label="🤒 Posts (Client + Zustand)" />
+        <LinkButton href="/post-client" label="🤓 Posts (Client + Zustand)" />
         <LinkButton href="/comments/1" label="💬 Comments Post 1" />
         <LinkButton href="/comments/8" label="💬 Comments Post 8" />
         <LinkButton href="/comments/10" label="💬 Comments Post 10" />
@@ -29,7 +29,7 @@ function LinkButton({ href, label }: LinkButtonProps) {
   return (
     <Link
       href={href}
-      className="block w-full text-center bg-grey-500 text-white hover:bg-grey-400 font-semibold py-3 px-6 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out tracking-wide"
+      className="block w-full text-center bg-yellow-500 text-white hover:bg-yellow-400 font-semibold py-3 px-6 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out tracking-wide"
     >
       {label}
     </Link>
